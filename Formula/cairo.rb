96e9c151ebef52c925732c7929b3c71fd1fd3c89
Axel <axel.rozobrezillac@gmail.com>
2018-07-29 18:43:16 +0200

    Move patches

--- Formula/cairo.rb
+++ Formula/cairo.rb
@@ -19,7 +19,8 @@ class Cairo < Formula
   depends_on "glib"
 
   # Patch OpenGL header for macOS
-  patch :p0, <<-EOS.undent
+  opoo "dsjkfhskdhf"
+  patch :p0, %{
 diff --git a/src/cairo-gl-private.h b/src/cairo-gl-private.h
 index f02a58763..85a1e0512 100644
 --- a/src/cairo-gl-private.h
@@ -35,9 +36,9 @@ index f02a58763..85a1e0512 100644
  #endif
 
  #include "cairo-gl-ext-def-private.h"
-  EOS
+  }
 
-  patch :p0, <<-EOS.undent
+  patch :p0, %{
 diff --git a/configure.ac b/configure.ac
 index 5ee63a693..ae790d9fb 100644
 --- a/configure.ac
@@ -79,7 +80,7 @@ index 5ee63a693..ae790d9fb 100644
        if test -z "$FREETYPE_CONFIG"; then
          AC_PATH_PROG(FREETYPE_CONFIG, freetype-config, no)
        fi
-  EOS
+  }
 
   def install
     if build.head?
